bd79bb6626ae7cf442d6e7cb827412fcc22cd8b3
Ryan Berdeen <ryan@ryanberdeen.com>
2012-12-21 14:45:53 -0500

    use root folder if public doesn't exist

--- app.rb
+++ app.rb
@@ -4,7 +4,9 @@ require "bundler/setup"
 Bundler.require(:default)
 
 configure do
-  set :public_folder, File.join(settings.local_dir, 'public')
+  public_folder = File.join settings.local_dir, 'public'
+  public_folder = settings.local_dir unless File.exist? public_folder
+  set :public_folder, public_folder
   sprockets = Sprockets::Environment.new
   sprockets.append_path settings.public_folder
   set :sprockets, sprockets
